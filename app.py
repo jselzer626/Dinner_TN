1ef232fcaef46d61d6212963a3a0c84fa037d756
Jon Selzer <jselzer626@gmail.com>
2020-07-29 00:33:45 -0400

    added env variables

---
+++ app.py
@@ -0,0 +1,17 @@
+import os
+import json
+from dotenv import load_dotenv
+from flask import Flask, render_template, jsonify, request
+
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+load_dotenv()
+
+api_key = os.environ.get("api_key")
+print(api_key)
+
+@app.route('/')
+def home():
+    return "Yo!!!!"
+    
