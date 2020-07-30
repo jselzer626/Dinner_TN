b175845cb40151f75c8eb12c74db3938d73c1a88
Jon Selzer <jselzer626@gmail.com>
2020-07-29 20:02:04 -0400

    depedencies

--- app.py
+++ app.py
@@ -3,15 +3,13 @@ import json
 from dotenv import load_dotenv
 from flask import Flask, render_template, jsonify, request
 
-
 app = Flask(__name__)
 app.config['DEBUG'] = True
 load_dotenv()
 
 api_key = os.environ.get("api_key")
-print(api_key)
 
 @app.route('/')
 def home():
-    return "Yo!!!!"
+    
     
