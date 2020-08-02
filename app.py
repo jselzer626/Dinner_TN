a2bd4332383451161403cfdedcd21879ca6c607b
Jon Selzer <jselzer626@gmail.com>
2020-08-02 16:14:42 -0400

    twilio SMS code added

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
 import os
 import json
 from dotenv import load_dotenv
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, jsonify, request
+from twilio.rest import Client
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 load_dotenv()
 
-api_key = os.environ.get("api_key")
+client = Client(os.environ.get("ACCOUNT_SID"), os.environ.get("AUTH_TOKEN"))
+origin_number = '+12057829884'
 
 @app.route('/')
-def home():
+def sendSMS():
+    if request.method == "POST":
+        destination = request.form['number']
+        recipe = request.form['recipe']
+        ingredients = request.form['ingredients']
+
+        message = client.messages.create(
+            body=recipe
+            from_=origin_number,
+            to=f"+1{destination}"
+        )
+
+        return jsonify(message.status)
     
-    
+    return 'hello world'
