fdb745729ebad4d833eecb4399d516d7057a8dab
Jon Selzer <jselzer626@gmail.com>
2020-08-03 14:59:52 -0400

    more heroku configuration

--- app.py
+++ app.py
@@ -32,3 +32,7 @@ def sendSMS():
         return jsonify(message.status)
 
     return 'hello world'
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
