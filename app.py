f359ac2e4d43e6995ebac9ee0fa41c2057cef555
Jon Selzer <jselzer626@gmail.com>
2020-08-07 19:32:05 -0400

    moved recipe api to server

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import json
+import requests
 from dotenv import load_dotenv
 from flask import Flask, jsonify, request
 from twilio.rest import Client
@@ -21,21 +22,25 @@ def sendSMS():
         recipe = request.form['recipe']
         ingredients = request.form['ingredients']
 
+        # twilio has a 1600 character limit so checking values before creating request
+        if len(recipe) > 1600 or len(ingredients) > 1600:
+            messageSuccess = False
+
     # twilio 
-    # twilio has a 1600 character limit so splitting into two messages
-        try:
-            ingredientsMessage = client.messages.create(
-                body=f"Ingredients: {ingredients}",
-                from_=origin_number,
-                to=f"+1{destination}"
+        if messageSuccess:
+            try:
+                ingredientsMessage = client.messages.create(
+                    body=f"Ingredients: {ingredients}",
+                    from_=origin_number,
+                    to=f"+1{destination}"
+                    )
+                recipeMessage = client.messages.create(
+                    body=f"Directions: {recipe}",
+                    from_=origin_number,
+                    to=f"+1{destination}"    
                 )
-            recipeMessage = client.messages.create(
-                body=f"Directions: {recipe}",
-                from_=origin_number,
-                to=f"+1{destination}"    
-            )
-        except Exception:
-            messageSuccess = False 
+            except Exception:
+                messageSuccess = False 
 
         response = jsonify(messageSuccess)
         # CORS
@@ -45,6 +50,55 @@ def sendSMS():
     return 'hello world'
 
 
+@app.route('/searchRecipes', methods=['GET'])
+def searchRecipes():
+
+    if request.method == "GET":
+
+        print(spoonacular)
+
+        response = ''
+        searchParams = request.args.get('query')
+        requestParams = {
+            'query':searchParams,
+            'instructionsRequired': True,
+            'apiKey':os.environ.get("SPOONACULAR_KEY"),
+            'number':100
+        }
+        url = "https://api.spoonacular.com/recipes/search"
+
+        try:
+            r = requests.get(url, requestParams)
+            response = jsonify(r.json())
+        except Exception:
+            response = jsonify('error')
+
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        return response
+
+@app.route('/getRecipeDetails/<int:recipeId>', methods=['GET'])
+def getRecipeDetails(recipeId):
+
+    if request.method == 'GET':
+
+        response = ''
+        requestParams = {
+            'includeNutrition':False,
+            'apiKey':os.environ.get("SPOONACULAR_KEY")
+        }
+        url = f"https://api.spoonacular.com/recipes/{recipeId}/information"
+
+        # print(requests.get(url, requestParams).json())
+
+        try:
+            r = requests.get(url, requestParams)
+            response = jsonify(r.json())
+        except Exception:
+            response = jsonify('error')
+
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        return response
+        
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
     app.run(threaded=True, port=5000)
